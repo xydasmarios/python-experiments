2d186f9042d5394ddb73056fde5df00f6b8c455f
xmarios <xydasmarios@gmail.com>
2019-04-25 10:46:16 +0300

    testing branch

--- fundamentals.py
+++ fundamentals.py
@@ -1,1 +1,43 @@
-print("Hello")
+# h = 50
+# if h > 50:
+#     print("x")
+# elif h < 20:
+#     print("xx")
+# else:
+#     print("xxx")
+
+
+# c = 5
+# while c != 0:
+#     print(c)
+#     c -= 1
+
+# c = 5
+# while c:
+#     print(c)
+#     c -= 1
+
+
+# d = {"alice": '1111'}
+# #print(d)
+#
+# d['alice'] = '2222'
+# #print(d)
+#
+# d['charles'] = '3333'
+# print(d)
+
+
+# cities = ["London", "New York"]
+# for city in cities:
+#     print(city)
+
+# cities = {"London": 1, "New York": 2}
+# for city in cities:
+#     print(city, cities[city])
+
+
+
+
+
+
