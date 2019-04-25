cf52b978fe7ad4850ff83de8e49bf2fc6e95ce11
xmarios <xydasmarios@gmail.com>
2019-04-25 15:52:06 +0300

    testing branch

--- fundamentals.py
+++ fundamentals.py
@@ -37,7 +37,43 @@
 #     print(city, cities[city])
 
 
+# def covert(s):
+#     """test"""
+#     x = -1
+#     try:
+#         x = int(s)
+#         print("succeeded", x)
+#     except ValueError:
+#         print("failed")
+#     except TypeError:
+#         print("failed2")
+#     return x
+
+# def covert(s):
+#     """test"""
+#     try:
+#         return int(s)
+#     except (ValueError, TypeError) as e:
+#         print("error: {}".format(str(e)))
+#         return -1
 
 
+class Person:
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
 
 
+class Student(Person):
+    def __init__(self, fname, lname):
+        Person.__init__(self, fname, lname)
+
+    def welcome(self):
+        print("welcome", self.firstname, self.lastname)
+
+
+x = Student("Mike", "Olsen")
+x.welcome()
